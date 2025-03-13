77b2a3cb4bf4a39e7fbcd832d0bba0143677b073
inum9 <inum9@outlook>
2025-03-14 00:51:21 +0530

    add createAccount method to AuthService for user registration

--- megaBlog/src/appwrite/auth.js
+++ megaBlog/src/appwrite/auth.js
@@ -9,6 +9,24 @@ export class AuthService{
         this.client.setEndpoint(config.appWriteUrl).setProject(config.appWriteProjectId);//this is the endpoint and project id
         
         this.Account=new Account(this.client);//this is the account
+
+
+        async function createAccount({,email,password,name}){
+           try {
+                 const userAccount=    await this.Account.create(ID.unique(),email,password,name);
+                  if(userAccount){
+                   //call another function to send email
+                  }else{
+                    return userAccount
+                  }
+
+           } catch (error) {
+            console.log(error);
+            
+           }
+
+           
+        }
     }
 
 }//this is the class that will be used to authenticate the user
