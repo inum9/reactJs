72901793d3025bbd9bc8c92422d2f9acf9a85815
inum9 <inum9@outlook>
2025-03-14 00:26:56 +0530

    initialize Vite + React project with essential configuration and structure

---
+++ megaBlog/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App(){
+  
+
+  console.log(import.meta.env.VITE_BLOG_APPWRITE);// it will return the value of VITE_BLOG_APPWRITE from .env file
+  
+  
+
+  return (
+
+  <>
+  <div>
+    <h1> code with appwrite !!!!</h1>
+  </div>
+  
+  </>
+  )
+}
+
+export default App
---
+++ megaBlog/src/conf/consf.js
@@ -0,0 +1,6 @@
+const config = {
+        appWriteUrl:String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+        appWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+        appWriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),   appWritebucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+export default config;
