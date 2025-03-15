23ca3ed4f55ba68dd7c1350761ea9dcccb5d0368
inum9 <inum9@outlook>
2025-03-15 15:38:24 +0530

    refactor AuthService class and fix createAccount method syntax

--- megaBlog/src/appwrite/auth.js
+++ megaBlog/src/appwrite/auth.js
@@ -1,38 +1,38 @@
 import config from "../conf/consf.js";
 
-import {Client,Account,ID} from 'appwrite';
+import { Client, Account, ID } from "appwrite";
 
-export class AuthService{
-    client=new Client();
-    Account;
-    constructor(){
-        this.client.setEndpoint(config.appWriteUrl).setProject(config.appWriteProjectId);//this is the endpoint and project id
-        
-        this.Account=new Account(this.client);//this is the account
+export class AuthService {
+  client = new Client();
+  Account;
+  constructor() {
+    this.client
+      .setEndpoint(config.appWriteUrl) //this is the endpoint
+      .setProject(config.appWriteProjectId); //this is the endpoint and project id
 
+    this.Account = new Account(this.client); //this is the account
+  }
+      //this is the function that will be used to create
+      // the user
+      async creatAccount({email,password,name}){
+        try {
+            const userAccount=  await this.Account.create(ID.unique(),email,password,name);
 
-        async function createAccount({,email,password,name}){
-           try {
-                 const userAccount=    await this.Account.create(ID.unique(),email,password,name);
-                  if(userAccount){
-                   //call another function to send email
-                  }else{
-                    return userAccount
-                  }
-
-           } catch (error) {
-            console.log(error);
-            
-           }
-
-           
+            if(userAccount){
+              return userAccount;
+            }else{
+              return null;
+            }
+        } catch (error) {
+          console.log(error);
+          
         }
-    }
+      };
+}
 
-}//this is the class that will be used to authenticate the user
+//this is the class that will be used to authenticate the user
 //this class will be used to authenticate the user
 
-const authService =new AuthService();
-
+const authService = new AuthService();
 
 export default authService;
