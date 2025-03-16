ceacdc69ac1d523092798e01685114fec53384fc
inum9 <inum9@outlook>
2025-03-16 20:07:39 +0530

    configured all steps about backend

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,22 +1,42 @@
 
+import { useEffect, useState } from 'react'
+import {useDispatch} from "react-redux";
 import './App.css'
+import authService from './appwrite/auth.js';
+import { login,logout } from './store/authSlice.js';
+import { Header,Footer } from './component/index.js';
+
 
 function App(){
   
+const [loading,setLoading]=useState(true);
 
-  console.log(import.meta.env.VITE_BLOG_APPWRITE);// it will return the value of VITE_BLOG_APPWRITE from .env file
-  
-  
 
-  return (
+const dispatch=useDispatch();
+ useEffect(()=>{
+  authService.getCurrentUser().then((res)=>{
+    if(res){
+      dispatch (login({res}));
+    }else{
+      dispatch(logout());
+    }
+  }).finally(()=>setLoading(false));
+ },[])
 
+  return ! loading? (
+     
   <>
-  <div>
-    <h1> code with appwrite !!!!</h1>
+ <div className='min-h-sc flex flex-wrap content content-between bg-gray-400'>
+  <div className='w-full block '>
+    <Header/>
+    <main>
+      
+    </main>
+    <Footer/>
   </div>
-  
+ </div>
   </>
-  )
+  ):null
 }
 
 export default App
