161d1e9c7f89a1e1186aa69680326e7a1ec88024
inum9 <inum9@outlook>
2025-03-26 21:54:52 +0530

    initialize the project

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,42 +1,17 @@
 
-import { useEffect, useState } from 'react'
-import {useDispatch} from "react-redux";
 import './App.css'
-import authService from './appwrite/auth.js';
-import { login,logout } from './store/authSlice.js';
-import { Header,Footer } from './component/index.js';
 
-
-function App(){
+function App() {
   
-const [loading,setLoading]=useState(true);
 
-
-const dispatch=useDispatch();
- useEffect(()=>{
-  authService.getCurrentUser().then((res)=>{
-    if(res){
-      dispatch (login({res}));
-    }else{
-      dispatch(logout());
-    }
-  }).finally(()=>setLoading(false));
- },[])
-
-  return ! loading? (
-     
-  <>
- <div className='min-h-sc flex flex-wrap content content-between bg-gray-400'>
-  <div className='w-full block '>
-    <Header/>
-    <main>
-      
-    </main>
-    <Footer/>
-  </div>
- </div>
-  </>
-  ):null
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+    
+  )
 }
 
 export default App
--- megaBlog/src/conf/consf.js
+++
@@ -1,8 +0,0 @@
-const config = {
-        appWriteUrl:String(import.meta.env.VITE_APPWRITE_ENDPOINT),
-        appWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-        appWriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),   appWritebucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-        appWriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-        
-}
-export default config;
