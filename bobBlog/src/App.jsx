02979682c87aa527edd16e813e190dc9f43aa6ed
inum9 <inum9@outlook>
2025-05-28 01:16:00 +0530

    implement the state management

--- bobBlog/src/App.jsx
+++ bobBlog/src/App.jsx
@@ -1,16 +1,37 @@
-
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
-
+import authService from './appwrite/auth.service.js';
+import {login,logout } from "../src/storeSTATE/authSlice.js"
+import {Header,Footer } from "../src/component/index.js";
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
