f5235e89a5da1d9ff62b39f1e2171c9d960e038e
inum9 <inum9@outlook>
2025-06-01 14:48:49 +0530

    implement the pages

--- bobBlog/src/component/index.js
+++ bobBlog/src/component/index.js
@@ -7,6 +7,12 @@ import Input from "./Input"
 import Select from "./Select";
 import Login from "./Login";
 import Button from "./Buttton"
+import AuthLayout from "./AuthLayout.jsx";
+import PostForm from './post-Form/PostForm.jsx'
+import PostCard from "./PostCard.jsx";
+import Rte from "./Rte.jsx";
+import Signup from "./SignUp.jsx";
 export {
-    Header,Footer,Container,LogoutBtn,Logo,Input,Select,Login,Button
+    Header,Footer,Container,LogoutBtn,Logo,Input,Select,Login,Button,AuthLayout,PostForm,PostCard,Rte,
+    Signup
 }
---
+++ bobBlog/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import { Login as loginComponent } from '../../src/component/index.js';
+function Login() {
+  return (
+    <div className='py-8'>
+        <loginComponent />
+    </div>
+  )
+}
+
+export default Login
